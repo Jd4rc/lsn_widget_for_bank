af6fccf030724735667b482e2d3c80220255c1a4
egor <e.nmitenin@gmail.com>
2026-03-19 20:07:59 +0300

    ran through linters src/main.py and src/widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,27 +1,30 @@
-def mask_account_card(par:str) -> str:
-    """" Returns the mask card number or account"""
+def mask_account_card(par: str) -> str:
+    """ " Returns the mask card number or account"""
     import re
+
     from src.masks import get_mask_account
     from src.masks import get_mask_card_number
 
-    foo = re.sub(r'[^\d+]', '', par)
-    bar = re.sub(r'[^\D\s$]', '', par)
+    foo = re.sub(r"[^\d+]", "", par)
+    bar = re.sub(r"[^\D\s$]", "", par)
 
     if len(foo) == 16:
         number = get_mask_card_number(int(foo))
     else:
         number = get_mask_account(int(foo))
 
-    mask = f'{bar}{number}'
+    mask = f"{bar}{number}"
 
     return mask
 
-def get_date(par:str) -> str:
-    """" Returns the format date"""
+
+def get_date(par: str) -> str:
+    """ " Returns the format date"""
     import re
+
     par = re.sub(
-        r'(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})T(\d{2}):(\d{2}):(\d{2}).(\d{6})',
-        r'\g<day>.\g<month>.\g<year>',
-        par
+        r"(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})T(\d{2}):(\d{2}):(\d{2}).(\d{6})",
+        r"\g<day>.\g<month>.\g<year>",
+        par,
     )
     return par
