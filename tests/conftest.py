1ac35b6689b5093085efdabf3190e8db177dbbf0
egor <e.nmitenin@gmail.com>
2026-04-05 20:14:56 +0300

    add fixtures for testing widget.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,3 +24,30 @@ def base_account_number_2():
 @pytest.fixture
 def base_account_number_3():
     return 73654108430135874789
+
+@pytest.fixture
+def base_mask_account_1():
+    return 'Счет 73654108430135874305'
+
+@pytest.fixture
+def base_mask_account_2():
+    return 'Счет не мой 73654108430135871505'
+
+@pytest.fixture
+def base_mask_account_3():
+    return 'Счет Олега 73654108430135870195'
+
+@pytest.fixture
+def base_mask_account_4():
+    return 'Maestro 7000792289609184'
+
+@pytest.fixture
+def base_mask_account_5():
+    return 'Visa Platinum 7000792289609854'
+
+@pytest.fixture
+def base_mask_account_6():
+    return 'Visa 7000792289609854'
+
+
+
