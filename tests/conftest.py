0fa2d4a1895a8565bdad42891619c044f6446438
egor <e.nmitenin@gmail.com>
2026-04-05 19:49:54 +0300

    init and add fixtures for mask funcs

---
+++ tests/conftest.py
@@ -0,0 +1,26 @@
+import pytest
+
+@pytest.fixture
+def base_card_number_1():
+    return 7000792289606361
+
+@pytest.fixture
+def base_card_number_2():
+    return 7000792289606591
+
+@pytest.fixture
+def base_card_number_3():
+    return 7000792289606123
+
+
+@pytest.fixture
+def base_account_number_1():
+    return 73654108430135874305
+
+@pytest.fixture
+def base_account_number_2():
+    return 73654108430135874123
+
+@pytest.fixture
+def base_account_number_3():
+    return 73654108430135874789
