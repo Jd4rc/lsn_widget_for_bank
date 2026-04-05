48f8f9455c8a9a8d427edfa3613f3143cb120d37
egor <e.nmitenin@gmail.com>
2026-04-05 19:50:48 +0300

    corrected parameter annotations and added checks

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,28 @@
-def get_mask_card_number(card_number: int) -> str:
+def get_mask_card_number(
+        card_number: int | str
+) -> str:
     """ " Returns the mask card number"""
     str_card_number = str(card_number)
-    return f"{str_card_number[:4]} {str_card_number[5:7]}** **** {str_card_number[-4:]}"
+
+    if not str_card_number.isdigit():
+        raise TypeError('card number must contain only digits')
+
+    if len(str_card_number) !=16:
+        raise ValueError('length must be 16')
+
+    return f"{str_card_number[:4]} {str_card_number[4:6]}** **** {str_card_number[-4:]}"
 
 
-def get_mask_account(mask_account: int) -> str:
+def get_mask_account(
+        mask_account: int | str
+) -> str:
     """ " Returns the mask account"""
     str_mask_account = str(mask_account)
+
+    if not str_mask_account.isdigit():
+        raise TypeError('account number must contain only digits')
+
+    if len(str_mask_account) !=20:
+        raise ValueError('length must be 20')
+
     return f"**{str_mask_account[-4:]}"
