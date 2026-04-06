455b420e6bc648040e4ffad337dbf091f192d0bc
egor <e.nmitenin@gmail.com>
2026-04-06 19:07:52 +0300

    add fixtures for testing func filter_by_state

--- tests/conftest.py
+++ tests/conftest.py
@@ -61,6 +61,39 @@ def base_date_2():
 def base_date_3():
     return '2021-01-02T02:26:18.671407'
 
+@pytest.fixture
+def base_data_for_filter_by_state_1():
+    return [
+            {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    ]
+
+@pytest.fixture
+def base_data_for_filter_by_state_2():
+    return [
+            {'id': 41428162, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 939719061, 'state': 'EXECUTED', 'date': '2016-03-30T02:08:58.425572'},
+            {'id': 594226850, 'state': 'CANCELED', 'date': '2011-09-12T21:27:25.241689'},
+            {'id': 615064000, 'state': 'EXECUTED', 'date': '2013-11-14T08:21:33.419441'}
+    ]
+
+@pytest.fixture
+def base_data_for_filter_by_state_3():
+    return [
+            {'id': 11128162, 'state': 'CANCELED', 'date': '2003-07-03T18:35:29.512364'},
+            {'id': 222719061, 'state': 'CANCELED', 'date': '2001-09-30T02:08:58.425572'},
+            {'id': 517226850, 'state': 'CANCELED', 'date': '2017-06-12T21:27:25.241689'},
+            {'id': 731064000, 'state': 'CANCELED', 'date': '2013-12-14T08:21:33.419441'}
+    ]
+
+@pytest.fixture
+def base_key_to_filter():
+    return 'CANCELED'
+
+
+
 
 
 
