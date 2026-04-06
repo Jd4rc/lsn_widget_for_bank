8d4fba107b04faf71b13178baae3cde40d991598
egor <e.nmitenin@gmail.com>
2026-04-06 20:01:30 +0300

    add tests for func sort_by_date

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,5 +1,5 @@
 import pytest
-from src.processing import filter_by_state
+from src.processing import filter_by_state, sort_by_date
 
 def test_filter_by_state_with_base_data_1(base_data_for_filter_by_state_1):
     assert filter_by_state(base_data_for_filter_by_state_1) == [
@@ -65,3 +65,61 @@ def test_filter_by_state_with_base_data_3_and_key_to_filter_par(base_data_for_fi
 ])
 def test_filter_by_state_with_parametrized(data, state, expected):
     assert filter_by_state(data, state) == expected
+
+
+@pytest.mark.parametrize('data, reverse, expected', [
+    (
+        [
+            {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+        ],
+        True,
+        [
+            {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+
+        ]
+    ),
+    (
+        [
+            {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+        ],
+        False,
+        [
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+            {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}
+        ]
+    )
+])
+def test_sort_by_date_with_parametrized(data, reverse, expected):
+    assert sort_by_date(data, reverse) == expected
+
+def test_sort_by_state_with_diff_time_format():
+    assert sort_by_date(
+        [
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+            {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03'}
+        ]
+    ) == [
+            {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018'}
+        ]
+
+def test_sort_by_state_with_empty_list():
+    assert sort_by_date([]) == []
+
+def test_sort_by_state_with_another_type_data():
+    assert sort_by_date({}) == []
