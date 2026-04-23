7e866e23ff65bec912f66ee668f434ded89028e4
egor <e.nmitenin@gmail.com>
2026-04-23 13:47:37 +0300

    add tests for transaction_descriptions

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,4 +1,4 @@
-from src.generators import filter_by_currency
+from src.generators import filter_by_currency, transaction_descriptions
 
 
 def test_filter_by_currency(base_transactions_in,base_transactions_out_1, base_transactions_out_2, base_transactions_out_3):
@@ -9,5 +9,8 @@ def test_filter_by_currency(base_transactions_in,base_transactions_out_1, base_t
 
 
 
-def test_transaction_descriptions():
-    assert ...
+def test_transaction_descriptions(base_transactions_in, base_descriptions_out_1, base_descriptions_out_2, base_descriptions_out_3):
+    i = transaction_descriptions(base_transactions_in)
+    assert next(i) == base_descriptions_out_1
+    assert next(i) == base_descriptions_out_2
+    assert next(i) == base_descriptions_out_3
