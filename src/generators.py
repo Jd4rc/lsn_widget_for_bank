9ee34ffc4b2898282dd90ba3686500f0680efc86
egor <e.nmitenin@gmail.com>
2026-04-23 10:00:56 +0300

    fix filter_by_currency

--- src/generators.py
+++ src/generators.py
@@ -4,6 +4,6 @@ def filter_by_currency(
         data: list[dict[str, int | str | dict[str, dict[str, str]]]],
         currency: str
 ) -> Iterator[dict[str, int | str | dict[str, str]]]:
-    yield from (x for x in data if x["operationAmount"]["currency"]["code"] == currency)
+    return (x for x in data if x["operationAmount"]["currency"]["code"] == currency)
 
 
