7c19f1b0d8d60c5f26d2e2d759689f306e12a4e7
egor <e.nmitenin@gmail.com>
2026-04-23 10:00:42 +0300

    fix tests for filter_by_currency

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,12 +1,13 @@
 from src.generators import filter_by_currency
 
 
-def test_filter_by_currency(base_transactions_in, base_transactions_out_1, base_transactions_out_2, base_transactions_out_3):
-    generator = filter_by_currency(base_transactions_in, 'USD')
-    assert next(generator) == base_transactions_out_1
-    assert next(generator) == base_transactions_out_2
-    assert next(generator) == base_transactions_out_3
+def test_filter_by_currency(base_transactions_in,base_transactions_out_1, base_transactions_out_2, base_transactions_out_3):
+    i = filter_by_currency(base_transactions_in, "USD")
+    assert next(i) == base_transactions_out_1
+    assert next(i) == base_transactions_out_2
+    assert next(i) == base_transactions_out_3
+
 
 
 def test_transaction_descriptions():
-    yield ...
+    assert ...
