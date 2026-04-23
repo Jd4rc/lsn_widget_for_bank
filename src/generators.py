f0d8d79e14a2496fd19422c7317aada4141befda
egor <e.nmitenin@gmail.com>
2026-04-23 09:42:12 +0300

    add func filter_by_currency

--- src/generators.py
+++ src/generators.py
@@ -1,2 +1,9 @@
-def some_func():
-    pass
+from collections.abc import Iterator
+
+def filter_by_currency(
+        data: list[dict[str, int | str | dict[str, dict[str, str]]]],
+        currency: str
+) -> Iterator[dict[str, int | str | dict[str, str]]]:
+    yield from (x for x in data if x["operationAmount"]["currency"]["code"] == currency)
+
+
