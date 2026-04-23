b49d359bf9d2d71e2b069f9ce232d63dc739419f
egor <e.nmitenin@gmail.com>
2026-04-23 09:41:26 +0300

    fix tests

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,12 +1,8 @@
 from src.generators import filter_by_currency
 
 
-def test_filter_by_currency_with_out_1(base_transactions_in, base_transactions_out_1):
-    assert next(filter_by_currency(base_transactions_in, 'USD')) == base_transactions_out_1
-
-def test_filter_by_currency_with_out_2(base_transactions_in, base_transactions_out_2):
-    assert next(filter_by_currency(base_transactions_in, 'USD')) == base_transactions_out_2
-
-
-def test_filter_by_currency_with_out_3(base_transactions_in, base_transactions_out_3):
-    assert next(filter_by_currency(base_transactions_in, 'USD')) == base_transactions_out_3
+def test_filter_by_currency(base_transactions_in, base_transactions_out_1, base_transactions_out_2, base_transactions_out_3):
+    generator = filter_by_currency(base_transactions_in, 'USD')
+    assert next(generator) == base_transactions_out_1
+    assert next(generator) == base_transactions_out_2
+    assert next(generator) == base_transactions_out_3
