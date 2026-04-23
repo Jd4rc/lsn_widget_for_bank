529b1be01b3e3d418f169edae165b0d21aae8efe
egor <e.nmitenin@gmail.com>
2026-04-23 09:46:04 +0300

    init test_transaction_descriptions

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -6,3 +6,7 @@ def test_filter_by_currency(base_transactions_in, base_transactions_out_1, base_
     assert next(generator) == base_transactions_out_1
     assert next(generator) == base_transactions_out_2
     assert next(generator) == base_transactions_out_3
+
+
+def test_transaction_descriptions():
+    yield ...
