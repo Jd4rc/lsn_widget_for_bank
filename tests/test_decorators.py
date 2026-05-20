ecca69dc069243e6adfd551b6b1c2ad85003b925
dosed <e.nmitenin@gmail.com>
2026-05-20 12:29:05 +0300

    Added a test for error logging

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -18,3 +18,20 @@ def test_log_returns_function_result(capsys):
     assert "Начало выполнения функции: add" in captured.out
     assert "Результат функции: 5" in captured.out
     assert "Конец выполнения функции: add" in captured.out
+
+
+def test_log_raises_exception_and_logs_it(capsys):
+    @log()
+    def divide(a, b):
+        return a / b
+
+    with pytest.raises(ZeroDivisionError):
+        divide(10, 0)
+
+    captured = capsys.readouterr()
+
+    assert "[ERROR]" in captured.out
+    assert "В функции divide возникла ошибка" in captured.out
+    assert "division by zero" in captured.out
+    assert "Входные параметры: (10, 0), {}" in captured.out
+    assert "Конец выполнения функции: divide" in captured.out
