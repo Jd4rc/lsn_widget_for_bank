2083c02d99166998dbb98fbfdd2d8d2f11869c92
dosed <e.nmitenin@gmail.com>
2026-05-20 11:30:11 +0300

    added time for logs

--- src/decorators.py
+++ src/decorators.py
@@ -1,27 +1,30 @@
 from functools import wraps
+from datetime import datetime
+
 
 def log(filename=''):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
             func_name = func.__name__
+            time = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
 
-            print(f"[LOG] Начало выполнения функции: {func_name}")
+            print(f"[LOG][{time}] Начало выполнения функции: {func_name}")
             try:
 
                 result = func(*args, **kwargs)
 
-                print(f"[LOG] Результат функции: {result}")
+                print(f"[LOG][{time}] Результат функции: {result}")
 
                 return result
 
             except Exception as e:
-                print(f"[ERROR] В функции {func_name} возникла ошибка: {e}")
+                print(f"[ERROR][{time}] В функции {func_name} возникла ошибка: {e}. Входные параметры: {args}, {kwargs}")
 
                 raise
 
             finally:
-                print(f"[LOG] Конец выполнения функции: {func_name}")
+                print(f"[LOG][{time}] Конец выполнения функции: {func_name}")
 
         return wrapper
 
