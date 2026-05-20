e1c43e69b8b19f4334e28e9111355f67b0760902
dosed <e.nmitenin@gmail.com>
2026-05-20 10:53:45 +0300

    Added code for logging to the console and the wraps module.

--- src/decorators.py
+++ src/decorators.py
@@ -1,6 +1,15 @@
+from functools import wraps
+
 def log(filename=''):
     def decorator(func):
+        @wraps(func)
         def wrapper(*args, **kwargs):
-            pass
+            print(f"[LOG] Начало выполнения функции: {func.__name__}")
+
+            result = func(*args, **kwargs)
+
+            print(f"[LOG] Конец выполнения функции: {func.__name__}")
+
+            return result
         return wrapper
     return decorator
