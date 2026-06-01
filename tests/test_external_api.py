d9c02d302ad560ae75d02301f45e23e10949ee9f
dosed <e.nmitenin@gmail.com>
2026-06-01 17:44:05 +0300

    add test for external api

---
+++ tests/test_external_api.py
@@ -0,0 +1,25 @@
+from unittest.mock import patch
+import pytest
+from src.utils.external_api import get_transaction_amount
+
+def test_get_transaction_amount_with_rub():
+    transaction = {
+        "id": 441945886,
+        "state": "EXECUTED",
+        "date": "2019-08-26T10:50:58.294041",
+        "operationAmount": {
+            "amount": "31957.58",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589"
+    }
+
+    result = get_transaction_amount(transaction)
+
+    assert result == 31957.58
+
