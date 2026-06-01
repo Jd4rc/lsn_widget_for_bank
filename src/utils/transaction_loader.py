d1299950807aa493bf43b2b549a06500ea2723fc
egor <e.nmitenin@gmail.com>
2026-06-01 12:09:56 +0300

    add transaction_loader

--- src/utils/transaction_loader.py
+++ src/utils/transaction_loader.py
@@ -1,2 +1,17 @@
-def load_transactions(filepath: str) -> list[dict]:
-    pass
+from pathlib import Path
+import json
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+def load_transactions(
+        filepath: str
+) -> list[dict]:
+    file_path = Path(BASE_DIR / filepath)
+
+    transactions = json.loads(
+        file_path.read_text(
+            encoding="utf-8"
+        )
+    )
+    return transactions
