fd18239116d7aa1aefb154dc5f0404d7edd66ff7
dosed <e.nmitenin@gmail.com>
2026-06-01 15:39:38 +0300

    add test for transac loader

--- tests/test_transaction_loader.py
+++ tests/test_transaction_loader.py
@@ -4,7 +4,7 @@ from unittest.mock import patch
 from src.utils.transaction_loader import load_transactions
 
 @patch('src.utils.transaction_loader.Path.read_text')
-def test_load_transactions(mock_read_text):
+def test_load_transactions_with_happy_path(mock_read_text):
     data = [
         {"id": 1},
         {"id": 2},
@@ -15,6 +15,21 @@ def test_load_transactions(mock_read_text):
     result = load_transactions('data/operations.json')
 
     assert result == data
+
+    mock_read_text.assert_called_with(
+        encoding="utf-8"
+    )
+
+
+@patch('src.utils.transaction_loader.Path.read_text')
+def test_load_transactions_with_empty_data(mock_read_text):
+    data = []
+
+    mock_read_text.return_value = json.dumps(data)
+
+    result = load_transactions('data/operations.json')
+
+    assert result == []
 
     mock_read_text.assert_called_with(
         encoding="utf-8"
