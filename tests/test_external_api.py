59f8a1420c835c53d1298b2ee13a324e300c7e02
dosed <e.nmitenin@gmail.com>
2026-06-01 18:19:00 +0300

    add test for external api

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -23,3 +23,30 @@ def test_get_transaction_amount_with_rub():
 
     assert result == 31957.58
 
+@patch('src.utils.external_api.requests.get')
+def test_get_transaction_amount_with_no_rub(mock_get):
+    transaction = {
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {
+            "amount": "9824.07",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702"
+    }
+
+    mock_response = mock_get.return_value
+    mock_response.json.return_value = {'result': 12300.1411}
+
+    result = get_transaction_amount(transaction)
+
+    assert result == 12300.1411
+    mock_response.raise_for_status.assert_called_once()
+
+
