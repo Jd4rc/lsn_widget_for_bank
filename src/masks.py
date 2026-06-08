f190cdc56bfe1d39656f82cbff537b0bd14611d4
dosed <e.nmitenin@gmail.com>
2026-06-08 14:09:55 +0300

    add logger to masks module

--- src/masks.py
+++ src/masks.py
@@ -4,15 +4,32 @@ logger = logging.getLogger(__name__)
 
 def get_mask_card_number(card_number: int | str) -> str:
     """ " Returns the mask card number"""
+
+    logger.info("Masking card number")
+
     str_card_number = str(card_number)
 
     if not str_card_number.isdigit():
+        logger.error("Card number contains non-digit characters")
         raise TypeError("card number must contain only digits")
 
     if len(str_card_number) != 16:
+        logger.error(
+            "Invalid card number length: %s",
+            len(str_card_number)
+        )
         raise ValueError("length must be 16")
 
-    return f"{str_card_number[:4]} {str_card_number[4:6]}** **** {str_card_number[-4:]}"
+
+    masked_number = (
+        f"{str_card_number[:4]}"
+        f" {str_card_number[4:6]}**"
+        f" **** {str_card_number[-4:]}"
+    )
+
+    logger.info("Card number masked successfully")
+
+    return masked_number
 
 
 def get_mask_account(mask_account: int | str) -> str:
