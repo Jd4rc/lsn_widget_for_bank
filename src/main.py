1d415c157703e9fa93cb92834e4450d81e612316
dosed <e.nmitenin@gmail.com>
2026-06-08 15:03:04 +0300

    add draft app run

---
+++ src/main.py
@@ -0,0 +1,15 @@
+# from src import logger_cfg
+#
+# from src.masks import get_mask_card_number
+# from src.utils import get_transaction_amount, load_transactions
+#
+# get_mask_card_number("1234567812345678")
+#
+# transactions = load_transactions(
+#     "data/operations.json"
+# )
+#
+# amounts = [
+#     get_transaction_amount(transaction)
+#     for transaction in transactions
+# ]
