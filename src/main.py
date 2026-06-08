00d261b14e2e55c42f40060c812d49def475efac
dosed <e.nmitenin@gmail.com>
2026-06-08 16:12:42 +0300

    update draft app run

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 # from src import logger_cfg
-#
+# import requests
 # from src.masks import get_mask_card_number
 # from src.utils import get_transaction_amount, load_transactions
 #
@@ -9,7 +9,29 @@
 #     "data/operations.json"
 # )
 #
-# amounts = [
-#     get_transaction_amount(transaction)
-#     for transaction in transactions
-# ]
+# amounts = []
+#
+# for transaction in transactions:
+#     if "operationAmount" not in transaction:
+#         continue
+#
+#     currency = transaction["operationAmount"]["currency"]["code"]
+#
+#     if currency != "RUB":
+#         continue
+#
+#     amount = get_transaction_amount(transaction)
+#     amounts.append(amount)
+#
+# print(amounts)
+#
+# for transaction in transactions:
+#     if "operationAmount" not in transaction:
+#         continue
+#
+#     currency = transaction["operationAmount"]["currency"]["code"]
+#
+#     if currency in ("USD", "EUR"):
+#         amount = get_transaction_amount(transaction)
+#         print(amount)
+#         break
