eec956cd39a3e2c786fd7e75c870b5b63c9182e7
dosed <e.nmitenin@gmail.com>
2026-06-08 15:02:25 +0300

    add logger cfg module

---
+++ src/logger_cfg.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+import logging
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+LOG_DIR = BASE_DIR / "logs"
+LOG_DIR.mkdir(exist_ok=True)
+
+LOG_FILE = LOG_DIR / "app.log"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=(
+        "%(asctime)s - "
+        "%(name)s - "
+        "%(levelname)s - "
+        "%(message)s"
+    ),
+    filename=LOG_FILE,
+    filemode="w",
+    encoding="utf-8",
+)
